8f0e9e49b51d17d71fa37b942aafeb4fc2e0888d
Sriharsha Reddy T <sriharshasriharsha83@gmail.com>
2026-02-13 08:51:26 +0530

    Add files via upload

---
+++ GreatestNumber.java
@@ -0,0 +1,18 @@
+package com.bnmit;
+
+public class GreatestNumber {
+
+    public static int findGreatest(int a, int b, int c) {
+        int greatest = a;
+
+        if (b > greatest) {
+            greatest = b;
+        }
+
+        if (c > greatest) {
+            greatest = c;
+        }
+
+        return greatest;
+    }
+}
